9a7e7de6b4a5bf109f3e72e41088526f1bae4bc5
samanthamarberger <slatham04@gmail.com>
2022-11-22 11:55:46 -0800

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header.js";
 import About from "./About.js";
+import ArticleList from "./ArticleList.js"
 
-console.log(blogData);
+console.log(blogData.posts);
 
 function App() {
   return (
     <div className="App">
       <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about} />
+      <ArticleList articles={blogData.posts} />
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Article from "./Article.js"
+
+function ArticleList({articles}) {
+    const articleElements = articles.map((article) => {
+        return (
+            <Article key={article.id} title={article.title} date={article.date} preview={article.preview} />
+        )
+    })
+    // console.log(articles);
+    return (
+        <main>
+            {articleElements}
+        </main>
+    )
+}
+
+export default ArticleList
