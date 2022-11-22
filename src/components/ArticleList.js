38b8adf4f37af9f3406a6bcd39393e572246427e
samanthamarberger <slatham04@gmail.com>
2022-11-22 12:25:22 -0800

    Finally done

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,13 @@
 import React from "react";
 import Article from "./Article.js"
 
-function ArticleList({articles}) {
-    const articleElements = articles.map((article) => {
-        return (
+function ArticleList({posts}) {
+    const articleElements = posts.map((article) => (
             <Article key={article.id} title={article.title} date={article.date} preview={article.preview} />
         )
-    })
-    // console.log(articles);
-    return (
-        <main>
-            {articleElements}
-        </main>
     )
+    console.log(articleElements);
+    return <main>{articleElements} </main>
 }
 
 export default ArticleList
